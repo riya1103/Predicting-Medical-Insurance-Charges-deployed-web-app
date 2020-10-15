e94c98cd4405ae5ad999300fa158ed03348aacf1
Riya Jain <46837551+riya1103@users.noreply.github.com>
2020-10-15 15:35:46 +0530

    Update webapp_insurance.py

--- webapp_insurance.py
+++ webapp_insurance.py
@@ -9,17 +9,8 @@ Original file is located at
 
 # It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to load
-
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
-import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+import pandas as pd
+import numpy as np
 import time
 import seaborn as sns
 
